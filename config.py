3e7c13d271a6fb1fe15aa70e9046cf2db54529ad
Parthi695 <rsahayaparthi2005@gmail.com>
2026-01-07 22:19:56 +0530

    Add configuration settings for streaming and database

---
+++ config.py
@@ -0,0 +1,10 @@
+STREAM_URL = "http://your_esp32_ip:81/stream"
+
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "YOUR_PASSWORD",
+    "database": "parking_db"
+}
+
+TESSERACT_PATH = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
